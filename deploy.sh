87b7313d49def69f40b4ab2da78e600ad0bfd67d
Waleboluwatife <waleboluwatife@gmail.com>
2025-10-22 15:50:47 +0100

    Added proxy-set config

--- deploy.sh
+++ deploy.sh
@@ -111,13 +111,16 @@ ssh -i "$SSH_KEY_PATH" "$SSH_USER@$SERVER_IP" <<EOF
 set -e
 sudo bash -c 'cat > /etc/nginx/conf.d/hng13_app.conf' <<CFG
 server {
-  listen 80;
-  server_name _;
-  location / {
-    proxy_pass http://127.0.0.1:$APP_PORT;
-    proxy_set_header Host \$host;
-    proxy_set_header X-Real-IP \$remote_addr;
-  }
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
 }
 CFG
 sudo nginx -t
